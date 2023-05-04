8e266d9b6a683b15e07494a50e6753fca8fd0349
Raushan <raushansharma7462@gmail.com>
2023-05-05 00:55:04 +0530

    professional-authenticator

---
+++ middleware/professionalMiddleware.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const secretKey = process.env.secret;
+var jwt = require('jsonwebtoken');
+const { professionalModel } = require("../module/professionalModule");
+
+async function AuthenicateProfessional(req, res, next) {
+    let token = req.headers.authorization;
+    // res.send(token)
+    jwt.verify(token, secretKey, async (err, decoded) => {
+        if (decoded) {
+            let professionalData = await professionalModel.findAll({where:{ "email": decoded.email }});
+            if (professionalData.length == 1) {
+                next();
+            } else {
+                res.send({"message": "Not Authorized"});
+            }
+        } else {
+            res.send([{ "message": "Not " }]);
+        }
+    });
+}
+
+module.exports = { AuthenicateProfessional };

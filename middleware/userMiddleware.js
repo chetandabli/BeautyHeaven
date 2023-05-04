dcc60b3fa9ed7524a486b147415779c51cfc773e
Raushan <raushansharma7462@gmail.com>
2023-05-05 00:52:34 +0530

    user-authenticator

---
+++ middleware/userMiddleware.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const secretKey = process.env.secret;
+var jwt = require('jsonwebtoken');
+const { UsersModel } = require("../module/userModule");
+
+async function AuthenicateUser(req, res, next) {
+    let token = req.headers.authorization;
+    jwt.verify(token, secretKey, async (err, decoded) => {
+        if (decoded) {
+            let userData = await UsersModel.findAll({where:{ "email": decoded.email }});
+            if (userData.length == 1) {
+                next();
+            } else {
+                res.send({"message": "Not Authorized"});
+            }
+        } else {
+            res.send([{ "message": "Not Authorized" }]);
+        }
+    });
+}
+
+module.exports = { AuthenicateUser };

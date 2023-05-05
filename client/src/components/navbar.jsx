370638120292a951a1b4c685a9983b6b4a0b93eb
chetandabli <hrku002@gmail.com>
2023-05-05 22:47:04 +0530

    route created

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Logo from "../assets/navbar_logo.png";
 import "./Footer.module.css"

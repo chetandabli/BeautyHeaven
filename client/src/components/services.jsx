5b30e9847b11a4fe21baedd49bc485c5f4588788
chetandabli <hrku002@gmail.com>
2023-05-07 08:59:42 +0530

    appended slots data to DOM

--- client/src/components/services.jsx
+++ client/src/components/services.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { useState, useEffect } from "react";
+import CSS from "./Services.module.css";
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 function Services() {
   const [slotData, setSlotData] = useState([]);
@@ -14,14 +17,55 @@ function Services() {
       });
       data = await data.json();
       console.log(data.beautyslot)
+      setSlotData(data.beautyslot)
     }
 
     fetchSlotData()
   }, []);
-  
+  // const ControlledPopup = (id) => {
+  //   const [open, setOpen] = useState(false);
+  //   const closeModal = () => setOpen(false);
+  //   console.log(id, "inside function")
+  //   return (
+  //     <div>
+  //       <button type="button" className="button" onClick={() => setOpen(o => !o)}>
+  //         Controlled Popup
+  //       </button>
+  //       <Popup open={open} closeOnDocumentClick onClose={closeModal}>
+  //         <div className="modal">
+  //           <a className="close" onClick={closeModal}>
+  //             &times;
+  //           </a>
+  //           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae magni
+  //           omnis delectus nemo, maxime molestiae dolorem numquam mollitia, voluptate
+  //           ea, accusamus excepturi deleniti ratione sapiente! Laudantium, aperiam
+  //           doloribus. Odit, aut.
+  //         </div>
+  //       </Popup>
+  //     </div>
+  //   );
+  // };
   return (
     <>
         <h1 style={{fontSize: "4rem", fontWeight: "bold", textAlign: "center", color: "#372415", paddingTop: "70px", margin: "0"}}>Professional Services</h1>
+        
+        <div className={CSS.mainContainer}>
+        {slotData?.map((el, index)=>{
+          return (
+            <>
+            <div className={CSS.card} key={el.id} style={{borderTop: "7px solid #AD9551"}}><p className={CSS.title}>{el.beautyType}</p><p className={CSS.professionalName} style={{color: "rgb(133, 136, 138)"}}>{el.professionalName}</p><p className={CSS.time} style={{color: "blue"}}>{el.bookingTime}</p><hr style={{color: "rgb(237, 237, 237)"}}></hr>
+            {/* {console.log(index)} */}
+            <button style={{backgroundColor: "#AD9551", border: "none", padding: "5px 15px", cursor: "pointer", marginLeft: "10px", marginBottom: "10px"}} onClick={()=>{
+              // console.log(el.id)
+              // ControlledPopup(el.id)
+            }}>Book</button>
+            </div>
+            </>
+          )
+        })}
+
+        </div>
+        <div id="popup-root" />
     </>
   )
 }

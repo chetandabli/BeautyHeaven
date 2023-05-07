40ed796a07faffb0a70bb486247d49ae7fec813e
chetandabli <hrku002@gmail.com>
2023-05-07 21:34:24 +0530

    added responsiveness to booking page

--- client/src/components/services.jsx
+++ client/src/components/services.jsx
@@ -1,11 +1,8 @@
-import React from 'react';
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import CSS from "./Services.module.css";
 import {
   useNavigate
 } from "react-router-dom";
-// import Popup from 'reactjs-popup';
-// import 'reactjs-popup/dist/index.css';
 
 function Services() {
   const [slotData, setSlotData] = useState([]);
@@ -16,7 +13,7 @@ function Services() {
         method: "GET",
         headers: {
           "authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imhya3UwMDJAZ21haWwuY29tIiwidXNlcm5hbWUiOiJjaGV0YW5kYWJsaSIsImlhdCI6MTY4MzM4OTgzMiwiZXhwIjoxNjgzOTk0NjMyfQ.7vHAeAF3xMiehbgNCGGExEvk09QTVYK6RCRP4WPZpoc"
-        }
+        },
       });
       data = await data.json();
       console.log(data.beautyslot)
@@ -25,29 +22,6 @@ function Services() {
 
     fetchSlotData()
   }, []);
-  // const ControlledPopup = (id) => {
-  //   const [open, setOpen] = useState(false);
-  //   const closeModal = () => setOpen(false);
-  //   console.log(id, "inside function")
-  //   return (
-  //     <div>
-  //       <button type="button" className="button" onClick={() => setOpen(o => !o)}>
-  //         Controlled Popup
-  //       </button>
-  //       <Popup open={open} closeOnDocumentClick onClose={closeModal}>
-  //         <div className="modal">
-  //           <a className="close" onClick={closeModal}>
-  //             &times;
-  //           </a>
-  //           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae magni
-  //           omnis delectus nemo, maxime molestiae dolorem numquam mollitia, voluptate
-  //           ea, accusamus excepturi deleniti ratione sapiente! Laudantium, aperiam
-  //           doloribus. Odit, aut.
-  //         </div>
-  //       </Popup>
-  //     </div>
-  //   );
-  // };
 
   const bookSlot = async(id)=>{
     let x = await fetch(`http://localhost:5000/users/bookingSlots/${id}`, {
@@ -59,7 +33,7 @@ function Services() {
     x = await x.json();
     console.log(x.message);
     console.log(x);
-if(x.message == "Add slot successfull"){
+if(x.message === "Add slot successfull"){
   navigate("/dashboard");
 }
   }
@@ -71,10 +45,7 @@ if(x.message == "Add slot successfull"){
         {slotData?.map((el)=>{
           return (
             <div className={CSS.card} key={el.id} style={{borderTop: "7px solid #AD9551"}}><p className={CSS.title}>{el.beautyType}</p><p className={CSS.professionalName} style={{color: "rgb(133, 136, 138)"}}>{el.professionalName}</p><p className={CSS.time} style={{color: "blue"}}>{el.bookingTime}</p><hr style={{color: "rgb(237, 237, 237)"}}></hr>
-            {/* {console.log(index)} */}
             <button style={{backgroundColor: "#AD9551", border: "none", padding: "5px 15px", cursor: "pointer", marginLeft: "10px", marginBottom: "10px"}} onClick={()=>{
-              // console.log(el.id)
-              // ControlledPopup(el.id)
               bookSlot(el.id)
             }}>Book</button>
             </div>

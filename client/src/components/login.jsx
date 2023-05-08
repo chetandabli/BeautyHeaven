fecef91c9a1411732691d2fda6dd9c955d38b12f
gouravsahuu <gsgourav.sahu@gmail.com>
2023-05-08 17:43:49 +0530

    added responsiveness to login signup page

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,11 +1,13 @@
 import styles from "./Login.module.css";
 import { Link } from "react-router-dom";
 import {useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 function Login () {
     const baseURL = "https://localhost:5000";
     const [emailData,setEmailData] = useState("");
     const [passData,setPassData] = useState("");
+    const navigate = useNavigate();
     
     function formSubmit(e) {
         e.preventDefault();
@@ -26,7 +28,18 @@ function Login () {
            return res.json();
         })
         .then((data) => {
-            console.log(data);
+            if(data.Access_Token){
+                const userDetails = {
+                    username : data.username,
+                    token : data.Access_Token
+                }
+                localStorage.setItem("userDetails",JSON.stringify(userDetails));
+                alert("Login Success");
+                navigate("/services");
+            }
+            else{
+                alert("Invalid Credentials !!");
+            }
         })
         .catch((err) => {
            console.log(err.message);
@@ -53,7 +66,7 @@ function Login () {
                 <input id="logpass" type="password" value={passData} onChange={(e) => setPassData(e.target.value)} placeholder="Please enter your Password" />
                 <p>This site is protected by reCAPTCHA and the Google Privacy Policy and Terms of Service apply.</p>
                 <div>
-                <Link style={{ fontWeight: "600", textDecoration: "underline" }} to={"/signup"}>Create New Account!</Link>
+                <Link style={{ fontWeight: "600", textDecoration: "underline" }} to={"/signup"}><p>Create New Account!</p></Link>
                 <input type="submit" value="Log In" />
                 </div>
             </form>

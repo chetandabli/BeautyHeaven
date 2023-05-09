1310c9fde2e843a3e0e23fdcfba08adb3734d089
chetandabli <hrku002@gmail.com>
2023-05-09 23:29:31 +0530

    deploy ready

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ const {adminRouter} = require('./router/admin.router')
 const {professionalRouter} = require("./router/professional.router")
 const {userRouter} = require("./router/user.router")
 const cors = require('cors')
+const path = require('path');
 
 require("dotenv").config()
 const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.use("/users",userRouter)
 app.use("/professions",professionalRouter)
 app.use('/admin', adminRouter)
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
 
 //connected to server
 const port = process.env.port || 3000

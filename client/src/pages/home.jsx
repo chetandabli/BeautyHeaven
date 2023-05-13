2a7644b46b22245ea55182194e4b0d0d5baced73
chetandabli <hrku002@gmail.com>
2023-05-13 17:20:04 +0530

    added responsiveness to navbar

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -8,7 +8,8 @@ function HomePage() {
     <>
     <Navbar/>
 <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh" }}>
-      <h1 style={{ fontSize: "6rem", fontWeight: "bold", textAlign: "center", color: "#372415" }}>Beauty Heaven</h1>
+      <h1 style={{ fontSize: "6rem", fontWeight: "bold", textAlign: "center", color: "#372415", marginBottom: "20px" }}>Beauty Heaven</h1>
+      <h3 style={{ fontSize: "2.5rem", fontWeight: "bold", textAlign: "center", color: "#AD9551" , marginTop: "0px", marginBottom: "30px"}}>Discover your perfect beauty match</h3>
       <Link to="/services" style={{ textDecoration: 'none' }}>
         <button style={{
           padding: "1rem 2rem", fontSize: "1.5rem", fontWeight: "bold", backgroundColor: "#AD9551", color: "#FFFFFF", border: "none", borderRadius: "5px", cursor: "pointer"

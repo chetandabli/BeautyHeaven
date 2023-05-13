c1284e67f636df707be9c79dcb8adab9bde36008
chetandabli <hrku002@gmail.com>
2023-05-13 12:11:20 +0530

    serving static file

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-const express  = require("express")
-const {seq,client} = require("./config/db")
-const {adminRouter} = require('./router/admin.router')
-const {professionalRouter} = require("./router/professional.router")
-const {userRouter} = require("./router/user.router")
-const cors = require('cors')
-const path = require('path');
+const express = require("express");
+const { seq, client } = require("./config/db");
+const { adminRouter } = require("./router/admin.router");
+const { professionalRouter } = require("./router/professional.router");
+const { userRouter } = require("./router/user.router");
+const cors = require("cors");
+const path = require("path");
 
-require("dotenv").config()
-const app = express()
-app.use(cors())
-app.use(express.json())
+require("dotenv").config();
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get("/", (req, res)=>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
 
-app.use("/users",userRouter)
-app.use("/professions",professionalRouter)
-app.use('/admin', adminRouter)
+app.use("/users", userRouter);
+app.use("/professions", professionalRouter);
+app.use("/admin", adminRouter);
 
-app.get('*', (req, res) => {
-    res.sendFile('index.html');
+// For any other route, serve the relevant file from the build directory
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
+// Connected to server
+const port = process.env.port || 3000;
+seq.sync().then(() => {
+  app.listen(port, () => {
+    console.log(`server is running on port ${port}\nhttp://localhost:${port}/`);
   });
-
-//connected to server
-const port = process.env.port || 3000
-seq.sync().then(()=>{
-    app.listen(port, ()=>{
-        console.log(`server is running on port ${port}\nhttp://localhost:${port}/`)
-    })
-})
+});
